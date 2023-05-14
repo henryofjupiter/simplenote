7e826b7b62dabb35a3dcad66542adccc8af5402c
henryofjupiter <oshardhenry@gmail.com>
2023-05-14 10:19:40 -0400

    refactors menuLoop & displayOptions, adds userInputValidation to manage user inputting display option

--- Notebook.java
+++ Notebook.java
@@ -8,17 +8,8 @@ public class Notebook {
     private String currTime;
 
 
-    public void menuLoop(Scanner scnr) {
-        int userInput;
-
-        while (true) {
-            displayOptions();
-            userInput = scnr.nextInt();
-
-        }
-    }
     // displays menu option to user
-    public void displayOptions() {
+    private void displayOptions() {
         System.out.println("Notebook Action Options");
         System.out.println("_____________________");
         System.out.println("Choose a action from the below options");
@@ -27,6 +18,51 @@ public class Notebook {
         System.out.println("3. create new file");
         System.out.println("4. Open a folder/ file");
         System.out.println("5. Delete a folder/ file");
-        System.out.println("6. Exit");
+        System.out.println("6. Show list of folders/ files");
+        System.out.println("7. Exit");
+    }
+
+    // handles the main function loop for input
+    // calls userInputValidation which is used to handle input validation
+    public void menuLoop(Scanner scnr) {
+        String userInput;
+        while (true) {
+            displayOptions();
+            userInput = scnr.nextLine();
+            userInputValidation(userInput);
+        }
+    }
+
+    //handles userInput for menu option
+    private void userInputValidation(String userInput) {
+        if (userInput.equals("1")) {
+            System.out.println("Please Enter Your Name");
+
+        }
+        else if (userInput.equals("2")) {
+            System.out.println("Create a new folder");
+
+        }
+        else if (userInput.equals("3")) {
+            System.out.println("Create a new file");
+
+        }
+        else if (userInput.equals("4")) {
+            System.out.println("Open a folder/ file");
+
+        }
+        else if (userInput.equals("5")) {
+            System.out.println("Delete a folder/ file");
+
+        }
+        else if (userInput.equals("6")) {
+            System.out.println("Show list of folders/ files");
+        }
+        else if (userInput.equals("7")) {
+            System.out.println("Exit");
+        }
+        else {
+           System.out.println("Please choose menu option");
+        }
     }
 }
