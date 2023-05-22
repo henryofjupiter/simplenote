1b62c8d32a78cecdd8bf581c560501de0af7aa10
henryofjupiter <oshardhenry@gmail.com>
2023-05-22 04:41:42 -0400

    adds class usserInput.java to manage user input and other possible user input data moves code required for user name input from Notebook.java to userInput.java along with minor refactoring

---
+++ userInput.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class userInput extends Notebook {
+
+    userInput() {
+
+    }
+
+    public void getUserName(Scanner scnr)  {
+        System.out.println("Enter your name");
+        userName = scnr.nextLine();
+    }
+
+
+
+}
